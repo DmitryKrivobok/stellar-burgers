4f9f9536f8f5bffbc7b17c7f8f3be290b3081857
dmitry <krivobockd@yandex.ru>
2025-12-11 15:01:39 +0300

    burger-test-rev 3

--- cypress/e2e/order.cy.tsx
+++ cypress/e2e/order.cy.tsx
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Оформление заказа', () => {
+describe('Тесты бургерной', () => {
   beforeEach(() => {
     cy.setCookie('accessToken', 'fake-access-token');
     window.localStorage.setItem('refreshToken', 'fake-refresh-token');
@@ -22,8 +22,26 @@ describe('Оформление заказа', () => {
     cy.wait('@getUser');
   });
 
-  //работает
-  it('добавление ингредиентов в конструктор', () => {
+  it('открытие модального окна ингредиента', () => {
+    cy.contains('Детали ингредиента').should('not.exist');
+    cy.contains('Ингредиент 1', { timeout: 10000 }).click({ force: true });
+    cy.contains('Детали ингредиента', { timeout: 10000 }).should('exist');
+    cy.get('#modals')
+      .contains('Ингредиент 1', { timeout: 10000 })
+      .should('exist');
+  });
+
+  it('закрытие модального окна по крестику', () => {
+    cy.contains('Ингредиент 1').click({ force: true });
+    cy.get('#modals', { timeout: 15000 }).should('exist');
+    cy.get('#modals [data-cy=modal-close] svg')
+      .should('exist')
+      .click({ force: true });
+    cy.get('#modals').should('not.be.visible');
+    cy.contains('Детали ингредиента').should('not.exist');
+  });
+
+  it('оформление заказа', () => {
     cy.get('[data-cy=bun-ingredients]', { timeout: 10000 })
       .should('exist')
       .and('be.visible')
@@ -36,47 +54,6 @@ describe('Оформление заказа', () => {
       .contains('Добавить')
       .click({ force: true });
 
-    cy.get('[data-cy=constructor-bun-1]')
-      .contains('Ингредиент 1')
-      .should('exist');
-    cy.get('[data-cy=constructor-bun-2]')
-      .contains('Ингредиент 1')
-      .should('exist');
-
-    cy.get('[data-cy=constructor-ingredients]')
-      .should('exist')
-      .and('be.visible')
-      .contains('Ингредиент 2')
-      .should('exist');
-
-    cy.get('[data-cy=constructor-ingredients]')
-      .should('exist')
-      .and('be.visible')
-      .contains('Ингредиент 3')
-      .should('exist');
-  });
-
-  it('открытие модального окна ингредиента', () => {
-    cy.contains('Детали ингредиента').should('not.exist');
-    cy.contains('Ингредиент 1').click({ force: true });
-    cy.contains('Детали ингредиента').should('exist');
-    cy.get('#modals', { timeout: 10000 }).should('exist');
-    cy.get('#modals')
-      .contains('Ингредиент 1', { timeout: 10000 })
-      .should('exist');
-  });
-
-  it('закрывается по крестику', () => {
-    cy.contains('Ингредиент 2').click({ force: true });
-    cy.get('#modals', { timeout: 15000 }).should('exist');
-    cy.get('#modals [data-cy=modal-close]')
-      .should('exist')
-      .click({ force: true });
-    cy.get('#modals').should('not.be.visible');
-    cy.contains('Детали ингредиента').should('not.exist');
-  });
-
-  it('проверки что бургер в конструкторе', () => {
     cy.get('[data-cy^=constructor-bun-1]', { timeout: 15000 }).should(
       'contain.text',
       'Ингредиент 1'
@@ -90,35 +67,26 @@ describe('Оформление заказа', () => {
     cy.get('[data-cy^=constructor-ingredients]', { timeout: 15000 })
       .should('contain.text', 'Ингредиент 2')
       .and('contain.text', 'Ингредиент 3');
-  });
 
-  //работает
-  it('оформление заказа', () => {
-    cy.get('[data-cy=order-summ]', { timeout: 10000 })
+    cy.get('[data-cy=order-summ]')
       .should('be.enabled')
       .and('be.visible')
       .click({ force: true });
-  });
 
-  it('открытие и закрытие модального окна заказа', () => {
-    cy.get('#modals', { timeout: 10000 }).should('be.visible');
-    cy.contains('идентификатор заказа', { timeout: 10000 }).should(
-      'be.visible'
-    );
+    cy.get('#modals').should('exist');
+    cy.contains('идентификатор заказа').should('exist');
+    cy.get('[data-cy=order-number]').should('have.text', '12345');
+    cy.get('[data-cy=modal-close] svg').click({ force: true });
+    cy.get('#modals').should('not.be.visible');
 
-    cy.get('[data-cy=order-number]', { timeout: 10000 }).should(
-      'have.text',
-      '12345'
-    );
-
-    cy.get('[data-cy=modal-close]', { timeout: 10000 }).click({ force: true });
-    cy.get('#modals').should('not.exist');
-  });
-
-  //работает
-  it('проверка очистки конструктора', () => {
-    cy.get('[data-cy=constructor-bun-1]').should('not.exist');
-    cy.get('[data-cy=constructor-bun-2]').should('not.exist');
-    cy.get('[data-cy=constructor-ingredients]').should('not.exist');
+    cy.get('[data-cy^=constructor-ingredients]')
+      .contains('Ингредиент 1')
+      .should('not.exist');
+    cy.get('[data-cy^=constructor-ingredients]')
+      .contains('Ингредиент 2')
+      .should('not.exist');
+    cy.get('[data-cy^=constructor-ingredients]')
+      .contains('Ингредиент 3')
+      .should('not.exist');
   });
 });
